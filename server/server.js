f89e525ef9e4058032b229c75b2cfac7207b930a
llenk <ellenkeal@gmail.com>
2018-05-05 07:45:00 -0500

    database connected

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const pg = require('pg');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const pool = require('./modules/pool');
+
+app.use(bodyParser.json());
 
 app.use(express.static('server/public'));
+
+app.get('/properties', function(req, res) {
+    pool.query(`SELECT * FROM "properties"`)
+    .then(function(response) {
+        res.send(response);
+    }).catch(function(error) {
+        console.log(error);
+        res.sendStatus(500);
+    });
+});
 
 app.listen(PORT, function() {
     console.log(`listening on port ${PORT}`);
