3947a3fedf90f4115d161e7eb7821b360a0beee1
llenk <ellenkeal@gmail.com>
2018-05-05 19:31:06 -0500

    add new property

--- server/public/scripts/controllers/home.controller.js
+++ server/public/scripts/controllers/home.controller.js
@@ -1,3 +1,32 @@
 app.controller('homeController', ['$http', function($http) {
     var self = this;
+    self.newProperty = {
+        city: '',
+        cost: '',
+        sqft: '',
+        type: '',
+        image_path: ''
+    };
+
+    self.listProperty = function() {
+        console.log(self.newProperty);
+        $http({
+            method: 'POST',
+            url: '/properties',
+            data: self.newProperty
+        }).then(function(response) {
+            console.log(response);
+        }).catch(function(error) {
+            console.log(error);
+        });
+
+        //reset values
+        self.newProperty = {
+            city: '',
+            cost: '',
+            sqft: '',
+            type: '',
+            image_path: ''
+        };
+    };
 }]);
